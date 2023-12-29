5099d89d8c0a8b8aafce45fca1e7de1307ec99d9
Robert Hafner <tedivm@tedivm.com>
2023-12-28 21:47:38 -0600

    comments and cleanup

--- paracelsus/cli.py
+++ paracelsus/cli.py
@@ -1,14 +1,10 @@
 import importlib
-import os
 import sys
 from enum import Enum
 from pathlib import Path
-from pprint import pprint
 from typing import List
 
 import typer
-from alembic.config import Config
-from alembic.script import ScriptDirectory
 from typing_extensions import Annotated
 
 from .transformers.dot import Dot
@@ -16,13 +12,19 @@ from .transformers.mermaid import Mermaid
 
 app = typer.Typer()
 
-transformers = {"mmd": Mermaid, "mermaid": Mermaid, "dot": Dot}
+transformers = {
+    "mmd": Mermaid,
+    "mermaid": Mermaid,
+    "dot": Dot,
+    "gv": Dot,
+}
 
 
 class Formats(str, Enum):
     mermaid = "mermaid"
     mmd = "mmd"
     dot = "dot"
+    gv = "gv"
 
 
 @app.command()
@@ -48,19 +50,23 @@ def graph(
         Formats, typer.Option(help="The file format to output the generated graph to.")
     ] = Formats.mermaid,
 ):
+    # Update the PYTHON_PATH to allow more module imports.
     for dir in python_dir:
         sys.path.append(dir)
 
     # Import the base class so the metadata class can be extracted from it.
+    # The metadata class is passed to the transformer.
     module_path, class_name = base_class_path.split(":", 2)
     base_module = importlib.import_module(module_path)
     base_class = getattr(base_module, class_name)
     metadata = base_class.metadata
 
-    # The modules holding the model classes have to be imported to get put in the
-    # metaclass model registry.
+    # The modules holding the model classes have to be imported to get put in the metaclass model registry.
+    # These modules aren't actually used in any way, so they are discarded.
+    # They are also imported in scope of this function to prevent namespace pollution.
     for module in import_module:
         if ":*" in module:
+            # Sure, execs are gross, but this is the only way to dynamically import wildcards.
             exec(f"from {module[:-2]} import *")
         else:
             importlib.import_module(module)
@@ -70,8 +76,11 @@ def graph(
         raise ValueError(f"Unknown Format: {format.value}")
     transformer = transformers[format.value]
 
-    # Output the graph structure.
-    typer.echo(str(transformer(metadata)))
+    # Save the graph structure to string.
+    graph_output = str(transformer(metadata))
+
+    #
+    typer.echo(graph_output)
 
 
 @app.command()
