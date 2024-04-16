a29adbd85acd221f716def316d7c8e155fb1ad6d
Robert Hafner <tedivm@tedivm.com>
2024-04-15 19:35:33 -0700

    more backports in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 import tempfile
-from datetime import UTC, datetime
+from datetime import datetime, timezone
 from pathlib import Path
 from uuid import uuid4
 
@@ -9,6 +9,7 @@ import pytest
 from sqlalchemy import Boolean, DateTime, ForeignKey, String, Text, Uuid
 from sqlalchemy.orm import declarative_base, mapped_column
 
+UTC = timezone.utc
 
 @pytest.fixture
 def metaclass():
